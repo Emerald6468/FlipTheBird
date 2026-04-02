b91694b0b2b52cd29a034f8459f45c046581b370
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-04-02 13:37:15 -0500

    inprog player

--- flip,-the-bird/Scripts/player.gd
+++ flip,-the-bird/Scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+@export var Speed = 20.0
+@export var Force = 21
 const JUMP_VELOCITY = 4.5
 
 
@@ -16,13 +17,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * Speed
+		velocity.z = direction.z * Speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, Speed)
+		velocity.z = move_toward(velocity.z - Force, 0, Speed)
 
 	move_and_slide()
