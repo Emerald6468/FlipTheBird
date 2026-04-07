eab26d8fa394f15908c38d9926c9bea7b91d160d
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-04-06 19:20:53 -0500

    movement:)

--- flip,-the-bird/Scripts/player.gd
+++ flip,-the-bird/Scripts/player.gd
@@ -2,28 +2,79 @@ extends CharacterBody3D
 
 
 @export var Speed = 20.0
-@export var Force = 21
-const JUMP_VELOCITY = 4.5
+@export var Force = 100
+const JUMP_VELOCITY = 25
 
+#Slope
+var is_gripping = false
+var ignore = false
+@onready var slope_collider: ShapeCast3D = %SlopeCollider
+var slope_hit: Vector3
+var slope_angle
+var slope_normal: Vector3
 
+#player angle
+@onready var model: Node3D = $CollisionShape3D/Model
+var previous_angle
+var angle_power = 1.0
+
+@export var gravity_modifier = 5.0
+
+func _ready() -> void:
+	velocity.z = -Force
+	set_floor_snap_length(1.0)
+
+func SlopeSliding():
+	if slope_collider.is_colliding():
+		if !ignore:
+			is_gripping = true
+		slope_hit = slope_collider.get_collision_point(0)
+		slope_angle = get_floor_angle()
+		slope_angle = rad_to_deg(slope_angle)
+		print("slope angle " + str(slope_angle))
+		slope_normal = get_floor_normal()
+		var yInverse = 1 - slope_normal.y
+		var speed_increase = yInverse * slope_normal.z * angle_power * 1000
+		if speed_increase > 0: speed_increase = 0.3
+		print("speed increase " + str(speed_increase))
+		velocity.z += speed_increase
+
+func rotation_math():
+	var current_angle = model.get_rotation().x
+	if current_angle > 90 or current_angle < -90:
+		current_angle = -90
+	angle_power = (current_angle*.2)+1
+	
+
+		
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	velocity.z += .1
+	if velocity.z >= 0: velocity.z = -100
+	Global.forward_velocity = velocity.z
+	previous_angle = model.get_rotation().x
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		#leftandright
 		velocity.x = direction.x * Speed
-		velocity.z = direction.z * Speed
+		#rotate
+		var rad_change = deg_to_rad(input_dir.y)*2.5
+		model.rotate_x(rad_change)
 	else:
 		velocity.x = move_toward(velocity.x, 0, Speed)
-		velocity.z = move_toward(velocity.z - Force, 0, Speed)
-
+	rotation_math()
+	#gravity.
+	if is_on_floor():
+		SlopeSliding()
+	else:
+		if is_gripping:
+			pass
+			#print("test")
+			#apply_floor_snap()
+		velocity += (get_gravity() * gravity_modifier) * delta
+		velocity.y *= angle_power
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
 	move_and_slide()
--- flip,-the-bird/Scripts/ui.gd
+++ flip,-the-bird/Scripts/ui.gd
@@ -1,6 +1,7 @@
-extends Node
+extends Control
+@onready var velocity: Label = $CanvasLayer/Velocity
 
-var forward_velocity = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	velocity.text = "Forward Velocity: " + str(Global.forward_velocity)
