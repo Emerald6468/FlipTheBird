37220f6ac55f95b73834e183ad77bec9f7879b51
CanteloupeCrustacean <23gorrag@gmail.com>
2026-04-09 11:16:52 -0500

    Simple scoring system & improvements to rotating
    Added the base for a scoring system, and commented out code that was making the player launch into the sky while rotating. Also improved comment formatting, commented notes on current code, and ideas for later additions.

--- flip,-the-bird/Scripts/Global.gd
+++ flip,-the-bird/Scripts/Global.gd
@@ -1,8 +1,11 @@
 extends Node
 
 var forward_velocity = 0.0
+# Increases in intervals of 50 based on total flips + multiplier
+var score = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(score)
 	pass # Replace with function body.
 
 
